8fba9480958b54a0c6f650da195f906be97965da
sathishkumar-sk55 <sathishkumar.sk.0105@gmail.com>
2025-02-24 09:31:12 +0530

    fixed a bug, app crashing when we press math operator witout entering any value and added come button style

--- app/src/main/java/com/frost/learndemo3/MainActivity.kt
+++ app/src/main/java/com/frost/learndemo3/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.frost.learndemo3
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -10,7 +11,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -24,6 +27,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.frost.learndemo3.ui.theme.LearnDemo3Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,13 +38,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnDemo3Theme {
                 calculatorBody()
-                }
             }
         }
     }
+}
 
-fun calcuateResult(value1: Double, value2: Double, operator: Int): Double{
-    when{
+fun calcuateResult(value1: Double, value2: Double, operator: Int): Double {
+    when {
         operator == 1 -> return value1 + value2
         operator == 2 -> return value1 - value2
         operator == 3 -> return value1 * value2
@@ -50,13 +55,26 @@ fun calcuateResult(value1: Double, value2: Double, operator: Int): Double{
 
 
 @Composable
-fun calculatorBody(){
+fun calculatorBody() {
+
 
     val valueEntered = remember { mutableStateOf("") }
     val value1 = remember { mutableDoubleStateOf(0.0) }
     val operator = remember { mutableIntStateOf(0) }
     val result = remember { mutableStateOf("") }
 
+    @Composable
+    fun numberButton(onClick: () -> Unit, text: String) {
+        Button(
+            onClick,
+//            modifier = Modifier
+//                .padding(1.dp)
+//                .height(50.dp)
+//                .width(80.dp)
+        ) {
+            Text(text = text)
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -66,102 +84,105 @@ fun calculatorBody(){
         Text(text = "Calculator")
         OutlinedTextField(value = "${valueEntered.value}", onValueChange = {})
         Row {
-            Button(onClick = {valueEntered.value += "1"}){
-                Text(text = "1")
-            }
-            Button( onClick = {valueEntered.value += "2"}){
-                Text(text = "2")
-            }
-            Button(onClick = {valueEntered.value += "3"}){
-                Text(text = "3")
-            }
+            numberButton(onClick = { valueEntered.value += "1" }, text = "1")
+            numberButton(onClick = { valueEntered.value += "2" }, text = "2")
+            numberButton(onClick = { valueEntered.value += "3" }, text = "3")
             Button(onClick = {
-                value1.doubleValue = valueEntered.value.toDouble()
-                valueEntered.value = ""
-                operator.intValue = 1
-            }){
+                if (!valueEntered.value.isBlank()) {
+
+                    Log.d("MainActivity", "ValueEntereted not empty")
+                    value1.doubleValue = valueEntered.value.toDouble()
+                    valueEntered.value = ""
+                    operator.intValue = 1
+                }
+            }) {
                 Text(text = "+")
             }
         }
         Row {
-            Button(onClick = {valueEntered.value += "4"}){
-                Text(text = "4")
-            }
-            Button(onClick = {valueEntered.value += "5"}){
-                Text(text = "5")
-            }
-            Button(onClick = {valueEntered.value += "6"}){
-                Text(text = "6")
-            }
+            numberButton(onClick = { valueEntered.value += "4" }, text = "4")
+            numberButton(onClick = { valueEntered.value += "5" }, text = "5")
+            numberButton(onClick = { valueEntered.value += "6" }, text = "6")
             Button(onClick = {
-                value1.doubleValue = valueEntered.value.toDouble()
-                valueEntered.value = ""
-                operator.intValue = 2
-            }){
+                if (!valueEntered.value.isBlank()) {
+
+                    Log.d("MainActivity", "ValueEntereted not empty")
+                    value1.doubleValue = valueEntered.value.toDouble()
+                    valueEntered.value = ""
+                    operator.intValue = 2
+                }
+            }) {
                 Text(text = "-")
             }
         }
         Row {
-            Button(onClick = {valueEntered.value += "7"}){
-                Text(text = "7")
-            }
-            Button(onClick = {valueEntered.value += "8"}){
-                Text(text = "8")
-            }
-            Button(onClick = {valueEntered.value += "9"}){
-                Text(text = "9")
-            }
-            Button(onClick = {
-                value1.doubleValue = valueEntered.value.toDouble()
-                valueEntered.value = ""
-                operator.intValue = 3
-            }){
-                Text(text = "*")
-            }
+            numberButton(onClick = { valueEntered.value += "7" }, text = "7")
+            numberButton(onClick = { valueEntered.value += "8" }, text = "8")
+            numberButton(onClick = { valueEntered.value += "9" }, text = "9")
+            numberButton(onClick = {
+                if (!valueEntered.value.isBlank()) {
+
+                    Log.d("MainActivity", "ValueEntereted not empty")
+                    value1.doubleValue = valueEntered.value.toDouble()
+                    valueEntered.value = ""
+                    operator.intValue = 3
+                }
+            } ,text = "*")
+
         }
         Row(
 
             horizontalArrangement = Arrangement.End
         ) {
-            Button(onClick = {
-                value1.doubleValue = calcuateResult(value1.doubleValue, valueEntered.value.toDouble(), operator.value)
+            numberButton(onClick = {
+                value1.doubleValue = calcuateResult(
+                    value1.doubleValue,
+                    valueEntered.value.toDouble(),
+                    operator.value
+                )
                 result.value = value1.doubleValue.toString()
                 valueEntered.value = result.value
-            }){
-                Text(text = "=")
+            },text = "=")
             }
-            Button(onClick = {valueEntered.value += "0"}){
-                Text(text = "0")
+            numberButton(onClick = { valueEntered.value += "0" },text = "0")
             }
-            Button(onClick = {if(!valueEntered.value.contains('.')) {
-                valueEntered.value += "."
-            }}){
-                Text(text = ".")
-            }
-            Button(onClick = {
-                value1.doubleValue = valueEntered.value.toDouble()
-                valueEntered.value = ""
-                operator.intValue = 4
-            }){
-                Text(text = "/")
-            }
-        }
+            numberButton(onClick = {
+                if (!valueEntered.value.contains('.')) {
+                    if (!valueEntered.value.isBlank()) {
+
+                        Log.d("MainActivity", "ValueEntereted not empty")
+                        valueEntered.value += "."
+                    }
+                }
+            },text = ".")
+
+            numberButton(onClick = {
+                if (!valueEntered.value.isBlank()) {
+
+                    Log.d("MainActivity", "ValueEntereted not empty")
+                    value1.doubleValue = valueEntered.value.toDouble()
+                    valueEntered.value = ""
+                    operator.intValue = 4
+                }
+
+            },text = "/")
+
+
         Row(
 
             horizontalArrangement = Arrangement.End
         ) {
-            Button(onClick = {
-                value1.doubleValue = 0.0
-                valueEntered.value = ""
-                result.value = ""
-            }){
-                Text(text = "Clear")
+            numberButton(
+                onClick = {
+                    value1.doubleValue = 0.0
+                    valueEntered.value = ""
+                    result.value = ""
+                },
+                    text = "Clear"
+                )
             }
 
         }
-    }
-}
-
 
 
 
